579d5ed6acc6fb7f6175e6bf563099ae16d357b7
bruce414 <brucezhang414@gmail.com>
2024-11-27 15:07:52 +1300

    testing if git is working

---
+++ lib/frontend/components/bottom_nav_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+
+  late PersistentTabController _controller;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+}

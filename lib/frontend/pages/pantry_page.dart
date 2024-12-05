801230e6b886aa7aacfe4d1a2d92a08c85defb96
bruce414 <brucezhang414@gmail.com>
2024-12-05 22:02:12 +1300

    Worked on the FAB, it now displays two options with one leads to maunally add items and the other one lets the users to take pictures of the receipts

--- lib/frontend/pages/pantry_page.dart
+++ lib/frontend/pages/pantry_page.dart
@@ -10,32 +10,129 @@ class TrackerPage extends StatefulWidget {
 }
 
 class _TrackerPageState extends State<TrackerPage> {
+
+  void _showAddItemDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        var height = MediaQuery.of(context).size.height;
+        var width = MediaQuery.of(context).size.width;
+        return Stack(
+          children: [
+            Positioned(
+              top: height - 350,
+              left: width - 200,
+              child: Material(
+                color: Colors.transparent,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20.0)
+                  ),
+                  width: 170,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 12.0, bottom: 12.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: (){
+                            //pop out the dialog, then show the BS.
+                            Navigator.pop(context);
+                            showModalBottomSheet(
+                              context: context,
+                              useRootNavigator: true,
+                              isScrollControlled: true,
+                              backgroundColor: Colors.white,
+                              shape: const RoundedRectangleBorder(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(20.0),
+                                  topRight: Radius.circular(20.0),
+                                ),
+                              ),
+                              builder: (BuildContext context) {
+                                return const ManualAddFoodBs();
+                              },
+                            );
+                          },
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'manually add',
+                              ),
+                              SizedBox(
+                                width: 10.0,
+                              ),
+                              Icon(
+                                CupertinoIcons.add,
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Divider(
+                          color: Colors.black,
+                          thickness: 0.4,
+                          indent: 0,
+                          endIndent: 0,
+                        ),
+                        GestureDetector(
+                          onTap: (){},
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'take a picture',
+                              ),
+                              SizedBox(
+                                width: 10.0,
+                              ),
+                              Icon(
+                                CupertinoIcons.camera,
+                              ),
+                            ],
+                          )
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: const SafeArea(
         child: Padding(
           padding: EdgeInsets.all(20.0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 5.0, left: 5.0),
-                  child: Text(
-                    // 'Tracking...',
-                    'Pantry',
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.bold,
-                    ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 5.0, left: 5.0),
+                child: Text(
+                  // 'Tracking...',
+                  'Pantry',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                SizedBox(
-                  height: 260,
-                ),
-                Center(
+              ),
+              // SizedBox(
+              //   height: 260,
+              // ),
+              Expanded(
+                child: Center(
                   child: Column(
+                    mainAxisSize: MainAxisSize.min,
                     children: [
                       Image(
                         image: AssetImage('images/pantry.png'),
@@ -52,8 +149,8 @@ class _TrackerPageState extends State<TrackerPage> {
                     ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
@@ -62,21 +159,7 @@ class _TrackerPageState extends State<TrackerPage> {
         width: 75.0,
         child: FloatingActionButton(
           onPressed: (){
-            showModalBottomSheet(
-              context: context,
-              useRootNavigator: true,
-              isScrollControlled: true,
-              backgroundColor: Colors.white,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20.0),
-                  topRight: Radius.circular(20.0),
-                ),
-              ),
-              builder: (BuildContext context) {
-                return const ManualAddFoodBs();
-              },
-            );
+            _showAddItemDialog(context);
           },
           backgroundColor: Colors.orange,
           shape: RoundedRectangleBorder(

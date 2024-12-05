72b7e788bb11b236e92877bc5cfd3f2409cd247b
bruce414 <brucezhang414@gmail.com>
2024-12-05 22:03:29 +1300

    changed the add-food-items bottom sheet a bit

--- lib/frontend/components/manual_add_food_bs.dart
+++ lib/frontend/components/manual_add_food_bs.dart
@@ -8,31 +8,56 @@ class ManualAddFoodBs extends StatefulWidget {
 }
 
 class _ManualAddFoodBsState extends State<ManualAddFoodBs> {
+
+  Widget _addItemButton() {
+    return Container(
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: const Center(
+        child: Text(
+          'Add Item',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
+    return SizedBox(
       height: 600,
       child: Padding(
-        padding: EdgeInsets.all(30.0),
+        padding: const EdgeInsets.all(30.0),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisSize: MainAxisSize.min,
             children: [
-              Text(
+              const Text(
                 'Add item',
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 30.0,
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 30.0,
               ),
-              Text(
+              const Text(
                   'Varies based on things like your location, the \n'
                       'restaurant, the availability of nearby delivery people and whether you are an Uber One member.'
               ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: _addItemButton(),
+              )
             ],
           ),
         ),

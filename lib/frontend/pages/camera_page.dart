4771bec5dce8e38c712ed75548168751121e7366
bruce414 <brucezhang414@gmail.com>
2024-12-10 12:54:41 +1300

    got the camera working, still has a bit more to fix

--- lib/frontend/pages/camera_page.dart
+++ lib/frontend/pages/camera_page.dart
@@ -1,15 +1,89 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:food_inspection/frontend/state_management_models/camera_model.dart';
 
-class CameraPage extends StatefulWidget {
+class CameraPage extends ConsumerStatefulWidget {
   const CameraPage({super.key});
 
   @override
-  State<CameraPage> createState() => _CameraPageState();
+  _CameraPageState createState() => _CameraPageState();
 }
 
-class _CameraPageState extends State<CameraPage> {
+class _CameraPageState extends ConsumerState<CameraPage> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(cameraModel.notifier).initializeCamera();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final cameraState = ref.watch(cameraModel);
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: IconButton(
+                  onPressed: (){
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back_rounded,
+                    color: Colors.white,
+                    size: 30.0,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25.0,
+            ),
+            cameraState.isCameraInitialized ?
+            Center(
+                child: CameraPreview(cameraState.cameraController!)
+            ) :
+            const Center(
+              child: CircularProgressIndicator(),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            Center(
+              child: GestureDetector(
+                onTap: (){},
+                child: Container(
+                  height: 70,
+                  width: 70,
+                  decoration: const BoxDecoration(
+                    color: Colors.orange,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Center(
+                    child: Icon(
+                      CupertinoIcons.camera,
+                      color: Colors.white,
+                      size: 40.0,
+                    ),
+                  ),
+                ),
+              )
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
+
+
+
+
--- lib/frontend/state_management_models/camera_model.dart
+++ lib/frontend/state_management_models/camera_model.dart
@@ -2,56 +2,149 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:food_inspection/main.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-final cameraModel = ChangeNotifierProvider<CameraNotifier>((ref) => CameraNotifier());
+class CameraState {
+  final List<CameraDescription>? cameras;
+  final CameraController? cameraController;
+  final CameraDescription? mainCamera;
+  final bool isCameraInitialized;
 
-class CameraNotifier extends ChangeNotifier {
-  late List<CameraDescription>? _cameras;
-  CameraController? cameraController;
-  late CameraDescription? mainCamera;
-  bool isCameraInitialized = false;
+  // Constructor to initialize the state
+  CameraState({
+    this.cameras,
+    this.cameraController,
+    this.mainCamera,
+    this.isCameraInitialized = false,
+  });
+
+  // CopyWith method to update specific parts of the state
+  CameraState copyWith({
+    List<CameraDescription>? cameras,
+    CameraController? cameraController,
+    CameraDescription? mainCamera,
+    bool? isCameraInitialized,
+  }) {
+    return CameraState(
+      cameras: cameras ?? this.cameras,
+      cameraController: cameraController ?? this.cameraController,
+      mainCamera: mainCamera ?? this.mainCamera,
+      isCameraInitialized: isCameraInitialized ?? this.isCameraInitialized,
+    );
+  }
+}
+
+//State notifier approach
+class CameraNotifier extends StateNotifier<CameraState> {
+  CameraNotifier() : super(CameraState(isCameraInitialized: false));
+
+  //TODO: ask users permission to access the camera;
+  // void checkPermission() async {
+  //   PermissionStatus status = await PermissionHandler();
+  // }
 
   Future<void> initializeCamera() async {
     try {
-      //getting the list of cameras available on the device
-      _cameras = await availableCameras();
-      if (_cameras != null && _cameras!.isNotEmpty) {
-        //assign the first camera of the camera list to be the main camera (or the camera we will be using)
-        mainCamera = _cameras?[0];
-        //create the camera controller
-        cameraController = CameraController(mainCamera!, ResolutionPreset.max);
-      }
-      //initialize the camera controller
-      await cameraController!.initialize();
+      var cameras = await availableCameras();
+      if (cameras.isNotEmpty) {
+        print('cameras is not empty');
+        var mainCamera = cameras[0];
+        var cameraController = CameraController(mainCamera, ResolutionPreset.max);
 
-      isCameraInitialized = true;
-      notifyListeners();
+        await cameraController.initialize();
+
+        state = state.copyWith(
+          cameras: cameras,
+          cameraController: cameraController,
+          mainCamera: mainCamera,
+          isCameraInitialized: true,
+        );
+      } else {
+        throw Exception("No camera found");
+      }
     } catch (e) {
-      print("Trouble initializing the camera $e");
-      isCameraInitialized = false;
-      notifyListeners();
+      print("Trouble initializing the camera: $e");
+      state = state.copyWith(isCameraInitialized: false);
     }
   }
 
   Future<void> capturePhoto() async {
-    if (cameraController == null || !cameraController!.value.isInitialized) {
+    if (state.cameraController == null || !state.cameraController!.value.isInitialized) {
       throw Exception("Camera has not been initialized");
     }
-    await cameraController!.takePicture();
-    notifyListeners();
+
+    try {
+      final file = await state.cameraController!.takePicture();
+
+
+    } catch (e) {
+      print("Error capturing photo: $e");
+    }
   }
 
   void disposeCamera() {
-    cameraController?.dispose();
-    cameraController = null;
-    notifyListeners();
+    state.cameraController?.dispose();
+    state = state.copyWith(
+      cameraController: null,
+      mainCamera: null,
+      isCameraInitialized: false,
+    );
   }
-
-  @override
-  void dispose() {
-    disposeCamera();
-    super.dispose();
-  }
-
 }
+
+final cameraModel = StateNotifierProvider<CameraNotifier, CameraState>((ref) => CameraNotifier());
+
+//Change notifier approach
+// final cameraModel = ChangeNotifierProvider<CameraNotifier>((ref) => CameraNotifier());
+//
+// class CameraNotifier extends ChangeNotifier {
+//   late List<CameraDescription>? _cameras;
+//   CameraController? cameraController;
+//   late CameraDescription? mainCamera;
+//   bool isCameraInitialized = false;
+//
+//   Future<void> initializeCamera() async {
+//     try {
+//       //getting the list of cameras available on the device
+//       _cameras = await availableCameras();
+//       if (_cameras != null && _cameras!.isNotEmpty) {
+//         //assign the first camera of the camera list to be the main camera (or the camera we will be using)
+//         mainCamera = _cameras?[0];
+//         //create the camera controller
+//         cameraController = CameraController(mainCamera!, ResolutionPreset.max);
+//       }
+//       //initialize the camera controller
+//       await cameraController!.initialize();
+//
+//       isCameraInitialized = true;
+//       notifyListeners();
+//     } catch (e) {
+//       print("Trouble initializing the camera $e");
+//       isCameraInitialized = false;
+//       notifyListeners();
+//     }
+//   }
+//
+//   Future<void> capturePhoto() async {
+//     if (cameraController == null || !cameraController!.value.isInitialized) {
+//       throw Exception("Camera has not been initialized");
+//     }
+//     await cameraController!.takePicture();
+//     notifyListeners();
+//   }
+//
+//   void disposeCamera() {
+//     cameraController?.dispose();
+//     cameraController = null;
+//     notifyListeners();
+//   }
+//
+//   @override
+//   void dispose() {
+//     disposeCamera();
+//     super.dispose();
+//   }
+//
+// }
 
